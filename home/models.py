ff70304c32c3dceaf8b0c3b2e264faf3293ba975
Piyush Jha <piy0999@hku.hk>
2018-10-30 01:07:05 +0800

    order supplies (#1)

--- home/models.py
+++ home/models.py
@@ -2,3 +2,27 @@ from django.db import models
 
 class Warehouse(models.Model):
     name = models.CharField(max_length=200)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Item(models.Model):
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    shipping_weight_grams = models.PositiveIntegerField()
+
+class Clinic(models.Model):
+    name = models.CharField(max_length=200)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    status = models.CharField(max_length=50)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    time_placed = models.DateTimeField(auto_now_add=True, blank=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
