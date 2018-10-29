96ac1cf61d87be0cdfbfbdf96ffc09b5986a684d
Waqas Ali <waqas.abbasi@outlook.com>
2018-10-29 18:56:19 +0800

    Add supply table

--- supplies/urls.py
+++ supplies/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'supplies'
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- supplies/views.py
+++ supplies/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Item
+
 def index(request):
-    return HttpResponse("AS-P Supplies")
+    supplies_list = Item.objects.all()
+    context = {
+        'supplies_list': supplies_list,
+    }
+    return render(request, 'supplies/index.html', context)
